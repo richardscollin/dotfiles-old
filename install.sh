60010feaeee3b4cc046c3d15d16c840b4e2d24cc
Collin Richards <richardscollin@gatech.edu>
2015-03-25 00:59:02 -0400

    add local bin in path

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+CONFIG=$HOME/config
+
+
+mkdir -p $HOME/bin $HOME/.config/awesome $HOME/.vim/bundle
 
 #install Vundle for vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-CONFIG=$HOME/config
 ln -s $CONFIG/vimrc $HOME/.vimrc
 ln -s $CONFIG/rc.lua $HOME/.config/awesome/rc.lua
 ln -s $CONFIG/gitconfig $HOME/.gitconfig
