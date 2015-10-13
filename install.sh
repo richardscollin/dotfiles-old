18bf6be3e02e8a92c592064d49ebd6a09e117b15
Collin Richards <richardscollin@gatech.edu>
2015-10-12 22:41:34 -0400

    Add check to update repo's

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 CONFIG=$HOME/config
 
-
 mkdir -p $HOME/bin $HOME/.config/awesome $HOME/.vim/bundle
 
 #install Vundle for vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+VUNDLE_INSTALL=~/.vim/bundle/Vundle.vim
+if [ -d "$VUNDLE_INSTALL" ];then
+    pushd "$VUNDLE_INSTALL"
+    git pull
+    popd
+else
+    git clone https://github.com/gmarik/Vundle.vim.git "$VUNDLE_INSTALL"
+fi
 
 #install base16 shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+BASE16_INSTALL=~/.config/base16-shell
+if [ -d "$BASE16_INSTALL" ];then
+    pushd "$BASE16_INSTALL"
+    git pull
+    popd
+else
+    git clone https://github.com/chriskempson/base16-shell.git "$BASE_16"
+fi
 
 #install patched fonts
 #git clone https://github.com/powerline/fonts
