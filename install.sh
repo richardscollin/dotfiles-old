e5bb52c89222e3b897dc29b45b067f27369c16a8
Collin Richards <richardscollin@gatech.edu>
2015-10-21 00:55:26 -0400

    Rename directories

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+CONFIG=$HOME/g
 
-CONFIG=$HOME/config
-
-mkdir -p $HOME/bin $HOME/.config/awesome $HOME/.vim/bundle
+mkdir -p $HOME/b $HOME/.config/awesome $HOME/.vim/bundle
 
 function install_update_repo {
 # will install or update the git repo to the given directory
@@ -20,13 +19,14 @@ fi
 install_update_repo https://github.com/gmarik/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 install_update_repo https://github.com/chriskempson/base16-shell "$HOME/.config/base16-shell"
 install_update_repo https://github.com/powerline/fonts "$HOME/fonts"
+"$HOME/fonts/install.sh"
 
 #install patched fonts
 
 function mksymlink {
 # will make the symlink onlt if the file doesn't exist
 # mkksymlink target linkname
-if [ ! -f "$2" ];then
+if [ ! -e "$2" ];then
     ln -s "$1" "$2"
 fi
 }
