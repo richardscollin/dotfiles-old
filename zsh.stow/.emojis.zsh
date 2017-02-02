7c256e1fb0618cae8265375cac80de976d5fc8f0
Collin Richards <richardscollin@gatech.edu>
2017-02-02 00:34:23 -0500

    Add more emojis

--- zsh.stow/.emojis.zsh
+++ zsh.stow/.emojis.zsh
@@ -1,8 +1,31 @@
 
-alias 😸="cat "
+# I recomend using noto mono for glyph support
+typeset -A emojis
+emojis=(
+cat 😸
+cd 📂
+date 🗓
+firefox 🦊
+kill 🗡
+less 👓
+pacman ᗧ
+rm 🗑
+tee 🏌
+top 🎩
+touch 🖐
+tree 🌲
+vim 🖊
+)
+
+for k in "${(@k)emojis}";do
+    alias ${emojis[$k]}="$k "
+done
+
 on_space (){
-    emoji="😸"
-    LBUFFER="${LBUFFER/cat/$emoji} "
+    for k in "${(@k)emojis}";do
+        LBUFFER="${LBUFFER/$k/${emojis[$k]}}"
+    done
+    LBUFFER="${LBUFFER} "
 }
 zle -N on_space
 bindkey " " on_space
